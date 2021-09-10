d60a83a554e30d733f6d99a5115cc65b5af0cb6f
Adrien MARION <adrien.marion@epitech.eu>
2021-09-10 16:07:35 +0200

    Updating app title and removing useless stuff

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import {useAuthUser, useIsAuthenticated} from 'react-auth-kit'
 import Navbar from "../components/Navbar";
-import {useCookies} from "react-cookie";
 
 const Welcome = () => {
-    const [cookies] = useCookies();
-
     const auth = useAuthUser()
     const isAuthenticated = useIsAuthenticated()
 
-    // get token from cookie
-
-    console.log(cookies._auth)
     if (isAuthenticated()) {
         return <p>Bonjour {auth().user.login} ({auth().user.id})</p>
     } else {
         return <p>Bonjour</p>
     }
 }
-
 
 const Home = () => {
     return (
